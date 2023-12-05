43af51c4c0fdc0235525e1fd26ab6af448f9d424
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2023-12-05 23:03:29 +0530

    ADD: region selection

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,60 @@
+<script setup>
+import { onMounted, onUnmounted, ref } from "vue";
 import { useTheme } from "vuetify";
+import data from "../assets/data.json";
 const theme = useTheme();
+const allCountries = data;
+let countries = ref([]);
+let regions = ref([]);
+let selectedRegion = ref("");
+
+onMounted(() => {
+  allCountries = allCountries.map((country) => {
+    return {
+      name: "",
+      nativeName: "",
+      population: "",
+      region: "",
+      subRegion: "",
+      tld: "",
+      currencies: [],
+      languages: [],
+      borderCountries: [],
+      capital: "",
+    };
+  });
+  const mostPopulatedCountries = getMostPopulatedCountries();
+  countries.value = mostPopulatedCountries;
+  regions.value = getRegions();
+  selectedRegion.value = regions.value[0];
+});
+
+function getRegions() {
+  let tempRegions = [];
+  allCountries.forEach((country) => {
+    if (!tempRegions.includes(country.region)) {
+      tempRegions.push(country.region);
+    }
+  });
+  return tempRegions.sort();
+}
+function changeRegion(region) {
+  selectedRegion.value = region;
+  countries.value = allCountries.filter((country) => country.region === region);
+}
+function getMostPopulatedCountries() {
+  const top8 = allCountries
+    .sort((a, b) => b.population - a.population)
+    .slice(0, 8);
+  return top8;
+}
+function showDetails(country) {
+  console.log(country);
+}
 function toggleTheme() {
   theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
 }
+</script>
 
 <template>
   <div>
@@ -17,6 +69,16 @@ function toggleTheme() {
         }}</v-icon>
       </v-btn>
     </v-toolbar>
+      <v-col cols="2">
+        <v-select
+          :model-value="selectedRegion"
+          label="Filter by region"
+          @update:modelValue="changeRegion"
+          :items="regions"
+          hide-details
+          variant="solo"
+        ></v-select>
+      </v-col>
   </div>
 </template>
 
