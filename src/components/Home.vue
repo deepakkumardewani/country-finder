d43bcfe4b3b1df1516e31665eb9412995f826ecf
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2023-12-05 23:01:54 +0530

    ADD: dark/light mode

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,22 @@
-<script setup></script>
+import { useTheme } from "vuetify";
+const theme = useTheme();
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
 
 <template>
   <div>
-    <v-toolbar dark title="Country Finder"></v-toolbar>
+    <v-toolbar dark>
+      <v-toolbar-title class="ml-10 text-h5">Country Finder</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="toggleTheme" icon>
+        <v-icon>{{
+          theme.global.name.value === "dark"
+            ? "fa-solid fa-sun"
+            : "fa-solid fa-moon"
+        }}</v-icon>
+      </v-btn>
+    </v-toolbar>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -11,6 +11,21 @@ import "vuetify/styles";
 const vuetify = createVuetify({
   components,
   directives,
+  theme: {
+    defaultTheme: "dark",
+  },
 });
 
-createApp(App).use(vuetify).mount("#app");
+const app = createApp(App).use(vuetify).mount("#app");
+
+// const filters = {
+//   truncate(text, length, clamp) {
+//     clamp = clamp || "...";
+//     var node = document.createElement("div");
+//     node.innerHTML = text;
+//     var content = node.textContent;
+//     return content.length > length ? content.slice(0, length) + clamp : content;
+//   },
+// };
+
+// app.config.globalProperties.$filters = filters;
