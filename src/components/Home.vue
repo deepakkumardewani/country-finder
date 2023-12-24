51372a2902b5e733346e972c74f7d9f0668120cc
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2023-12-24 16:42:24 +0530

    ADD: transitions

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { onMounted, ref } from "vue";
-import { useTheme } from "vuetify";
+
 import Detail from "./Detail.vue";
+import CountryCard from "./CountryCard.vue";
 
 import Fuse from "fuse.js";
 
 import data from "../assets/data.json";
-const theme = useTheme();
+
 let allCountries = data;
 let searchQuery = ref("");
 let countries = ref([]);
@@ -25,15 +26,16 @@ onMounted(() => {
       languages = Object.values(country.languages);
     }
     if (country.currencies) {
-      currencies = Object.keys(allCountries[0].currencies)[0];
+      currencies = Object.keys(country.currencies)[0];
     }
+
     return {
       name: country.name.common,
       nativeName: country.name.nativeName,
       flag: country.flags.png,
       population: country.population,
       region: country.region,
-      subRegion: country.subregion,
+      subregion: country.subregion,
       tld: country.tld ? country.tld : [],
       currencies: currencies,
       languages: languages,
@@ -46,7 +48,7 @@ onMounted(() => {
   countries.value = mostPopulatedCountries;
   regions.value = getRegions();
   selectedRegion.value = regions.value[0];
-  sortValues = ["name", "region", "capital", "population"];
+  sortValues.value = ["name", "region", "capital", "population"];
   selectedSortType.value = "name";
   countries.value.sort((a, b) =>
     a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
@@ -104,106 +106,58 @@ function getMostPopulatedCountries() {
   return top8;
 }
 function showDetails(country) {
-  console.log(country);
   dialog.value = true;
   selectedCountry.value = country;
-}
-function toggleTheme() {
-  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
 }
 </script>
 
 <template>
   <div>
-    <div v-show="!dialog">
-      <v-toolbar dark>
-        <v-toolbar-title class="ml-10 text-h5">Country Finder</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn @click="toggleTheme" icon>
-          <v-icon>{{
-            theme.global.name.value === "dark"
-              ? "fa-solid fa-sun"
-              : "fa-solid fa-moon"
-          }}</v-icon>
-        </v-btn>
-      </v-toolbar>
-      <v-row class="mx-12 mt-10" justify="space-between">
-        <v-col cols="12" sm="6" lg="4">
-          <v-text-field
-            v-model="searchQuery"
-            @update:modelValue="searchCountry"
-            label="Search for a country"
-            hide-details
-            variant="solo"
-          ></v-text-field>
-        </v-col>
-        <v-spacer></v-spacer>
-        <v-col cols="12" sm="6" lg="3">
-          <v-select
-            :model-value="selectedSortType"
-            label="Sort by"
-            @update:modelValue="changeSort"
-            :items="sortValues"
-            hide-details
-            variant="solo"
-          ></v-select>
-        </v-col>
-        <v-col cols="12" sm="6" lg="3">
-          <v-select
-            :model-value="selectedRegion"
-            label="Filter by region"
-            @update:modelValue="changeRegion"
-            :items="regions"
-            hide-details
-            variant="solo"
-          ></v-select>
-        </v-col>
-      </v-row>
-
-      <div class="pa-10 d-flex flex-wrap justify-space-around">
-        <v-card
-          v-for="(country, index) in countries"
-          :key="index"
-          @click="showDetails(country)"
-          class="my-3"
-          height="350"
-          width="300"
-          :ripple="false"
-        >
-          <v-img
-            height="200"
-            :src="country.flag"
-            cover
-            class="text-white"
-          ></v-img>
-          <v-card-text>
-            <div class="mb-3 text-h6">
-              <b>{{ country.name }}</b>
-            </div>
-            <div>
-              <b>Population:</b>
-              {{
-                country.population.toLocaleString(undefined, {
-                  minimumFractionDigits: 2,
-                })
-              }}
-            </div>
-            <div><b>Region:</b> {{ country.region }}</div>
-            <div>
-              <b>Capital:</b>
-              {{ country.capital ? country.capital : "Unknown" }}
-            </div>
-          </v-card-text>
-        </v-card>
+    <v-slide-x-transition mode="in-out">
+      <div v-show="!dialog">
+        <v-row class="mx-12 mt-10" justify="space-between">
+          <v-col cols="12" sm="6" lg="4">
+            <v-text-field
+              v-model="searchQuery"
+              hide-details
+              label="Search for a country"
+              variant="solo"
+              @update:model-value="searchCountry"
+            ></v-text-field>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col cols="12" sm="6" lg="3">
+            <v-select
+              :model-value="selectedSortType"
+              label="Sort by"
+              :items="sortValues"
+              hide-details
+              variant="solo"
+              @update:model-value="changeSort"
+            ></v-select>
+          </v-col>
+          <v-col cols="12" sm="6" lg="3">
+            <v-select
+              :model-value="selectedRegion"
+              label="Filter by region"
+              :items="regions"
+              hide-details
+              variant="solo"
+              @update:model-value="changeRegion"
+            ></v-select>
+          </v-col>
+        </v-row>
+        <div class="pa-10 d-flex flex-wrap justify-space-around">
+          <CountryCard :countries="countries" :show-details="showDetails" />
+        </div>
       </div>
-    </div>
-
+    </v-slide-x-transition>
     <v-slide-x-transition mode="out-in">
       <Detail
-        @close="dialog = false"
         v-if="dialog"
         :country="selectedCountry"
         transition="scroll-x-transition"
+        @close="dialog = false"
       />
     </v-slide-x-transition>
   </div>
