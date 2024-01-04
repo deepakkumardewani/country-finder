3bbdcdebcafb592f7d003bcf668817ee2737856b
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-04 15:49:44 +0530

    remove unused code

--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import "./style.css";
+import "./styles.css";
 
 const app = createApp(App).use(vuetify).mount("#app");
-
-// const filters = {
-//   truncate(text, length, clamp) {
-//     clamp = clamp || "...";
-//     var node = document.createElement("div");
-//     node.innerHTML = text;
-//     var content = node.textContent;
-//     return content.length > length ? content.slice(0, length) + clamp : content;
-//   },
-// };
-
-// app.config.globalProperties.$filters = filters;
--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,8 @@ import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => {
-            return tag.startsWith("v-"); // (return true)
-          },
-        },
-      },
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
