86aa7e70ee162d8f361eb2552b34a3f108216aeb
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2024-01-04 15:50:08 +0530

    ADD: vitest config

---
+++ vitest.config.js
@@ -0,0 +1,25 @@
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => {
+            return tag.startsWith("v-");
+          },
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  test: {
+    environment: "happy-dom",
+  },
+});
